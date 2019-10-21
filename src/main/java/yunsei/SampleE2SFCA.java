cbb114cfcbb353ed911df8a56dc5b435e298242c
kwlee <kwlee@etri.re.kr>
2019-10-22 01:16:19 +0900

    .

---
+++ src/main/java/yunsei/SampleE2SFCA.java
@@ -0,0 +1,119 @@
+package yunsei;
+
+import static marmot.StoreDataSetOptions.*;
+import static marmot.optor.AggregateFunction.SUM;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+
+import common.SampleUtils;
+import marmot.DataSet;
+import marmot.GeometryColumnInfo;
+import marmot.Plan;
+import marmot.command.MarmotClientCommands;
+import marmot.optor.geo.advanced.Power;
+import marmot.optor.geo.advanced.WeightFunction;
+import marmot.plan.Group;
+import marmot.process.geo.E2SFCAParameters;
+import marmot.remote.protobuf.PBMarmotClient;
+import utils.CommandLine;
+import utils.CommandLineParser;
+import utils.StopWatch;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public class SampleE2SFCA {
+	private static final String BUS_SUPPLY = "연세대/강남구_버스";
+	private static final String SUBWAY_SUPPLY = "연세대/강남구_지하철";
+	private static final String FLOW_POP = "연세대/사업단실증/E2SFCA/유동인구";
+	private static final String RESULT_BUS = "tmp/E2SFCA/bus";
+	private static final String RESULT_SUBWAY = "tmp/E2SFCA/subway";
+	private static final String RESULT_CONCAT = "tmp/E2SFCA/concat";
+	private static final String RESULT = "tmp/result";
+	
+	public static final void main(String... args) throws Exception {
+//		PropertyConfigurator.configure("log4j.properties");
+		LogManager.getRootLogger().setLevel(Level.OFF);
+		
+		CommandLineParser parser = new CommandLineParser("Y2S_1 ");
+		parser.addArgOption("host", "ip_addr", "marmot server host (default: localhost)", false);
+		parser.addArgOption("port", "number", "marmot server port (default: 12985)", false);
+		
+		CommandLine cl = parser.parseArgs(args);
+		if ( cl.hasOption("help") ) {
+			cl.exitWithUsage(0);
+		}
+
+		String host = MarmotClientCommands.getMarmotHost(cl);
+		int port = MarmotClientCommands.getMarmotPort(cl);
+		
+		StopWatch watch = StopWatch.start();
+		
+		// 원격 MarmotServer에 접속.
+		PBMarmotClient marmot = PBMarmotClient.connect(host, port);
+		
+		Plan plan;
+		DataSet result;
+		
+		WeightFunction wfunc = Power.of(-0.1442);
+		
+		E2SFCAParameters params1 = new E2SFCAParameters();
+		params1.setConsumerDataset(FLOW_POP);
+		params1.setProviderDataset(BUS_SUPPLY);
+		params1.setOutputDataset(RESULT_BUS);
+		params1.setConsumerFeatureColumns("avg_08tmst,avg_15tmst");
+		params1.setProviderFeatureColumns("slevel_08,slevel_15");
+		params1.setOutputFeatureColumns("index_08,index_15");
+		params1.setServiceDistance(400);
+		params1.setWeightFunction(wfunc);
+		marmot.executeProcess("e2sfca", params1.toMap());
+	
+		E2SFCAParameters params2 = new E2SFCAParameters();
+		params2.setConsumerDataset(FLOW_POP);
+		params2.setProviderDataset(SUBWAY_SUPPLY);
+		params2.setOutputDataset(RESULT_SUBWAY);
+		params2.setConsumerFeatureColumns("avg_08tmst,avg_15tmst");
+		params2.setProviderFeatureColumns("slevel_08,slevel_15");
+		params2.setOutputFeatureColumns("index_08,index_15");
+		params2.setServiceDistance(800);
+		params2.setWeightFunction(wfunc);
+		marmot.executeProcess("e2sfca", params2.toMap());
+		
+		GeometryColumnInfo gcInfo = marmot.getDataSet(RESULT_BUS).getGeometryColumnInfo();
+		plan = marmot.planBuilder("append bus result")
+					.load(RESULT_BUS)
+					.project("the_geom,block_cd,index_08,index_15")
+					.store(RESULT_CONCAT, FORCE(gcInfo))
+					.build();
+		marmot.execute(plan);
+		result = marmot.getDataSet(RESULT_CONCAT);
+		
+		plan = marmot.planBuilder("append subway result")
+					.load(RESULT_SUBWAY)
+					.project("the_geom,block_cd,index_08,index_15")
+					.store(RESULT_CONCAT, APPEND)
+					.build();
+		marmot.execute(plan);
+		result = marmot.getDataSet(RESULT_CONCAT);
+		
+		plan = marmot.planBuilder("combine two results")
+					.load(RESULT_CONCAT)
+					.aggregateByGroup(Group.ofKeys("block_cd").tags("the_geom"),
+										SUM("index_08").as("index_08"),
+										SUM("index_15").as("index_15"))
+					.store(RESULT, FORCE(gcInfo))
+					.build();
+		marmot.execute(plan);
+		result = marmot.getDataSet(RESULT);
+		watch.stop();
+
+		SampleUtils.printPrefix(result, 5);
+		System.out.printf("elapsed=%s%n", watch.getElapsedMillisString());
+		
+		marmot.deleteDataSet(RESULT_CONCAT);
+		marmot.deleteDataSet(RESULT_SUBWAY);
+		marmot.deleteDataSet(RESULT_BUS);
+	}
+}
---
+++ src/main/java/yunsei/SampleE2SFCASetup.java
@@ -0,0 +1,69 @@
+package yunsei;
+
+import static marmot.StoreDataSetOptions.FORCE;
+import static marmot.optor.AggregateFunction.AVG;
+
+import org.apache.log4j.PropertyConfigurator;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+import common.SampleUtils;
+import marmot.DataSet;
+import marmot.GeometryColumnInfo;
+import marmot.MarmotRuntime;
+import marmot.Plan;
+import marmot.command.MarmotClientCommands;
+import marmot.plan.Group;
+import marmot.remote.protobuf.PBMarmotClient;
+import utils.StopWatch;
+
+/**
+ * 
+ * @author Kang-Woo Lee (ETRI)
+ */
+public class SampleE2SFCASetup {
+	private static final String FLOW_POP = "주민/유동인구/월별_시간대/2015";
+	private static final String SGG = "구역/시군구";
+	private static final String RESULT = "주민/유동인구/강남구/시간대/2015";
+	
+	public static final void main(String... args) throws Exception {
+		PropertyConfigurator.configure("log4j.properties");
+
+		// 원격 MarmotServer에 접속.
+		PBMarmotClient marmot = MarmotClientCommands.connect();
+		
+		StopWatch watch = StopWatch.start();
+		
+		Plan plan;
+		Geometry gangnaum = getGangnamGu(marmot);
+		
+		DataSet flowPop = marmot.getDataSet(FLOW_POP);
+		GeometryColumnInfo gcInfo = flowPop.getGeometryColumnInfo();
+		plan = marmot.planBuilder("강남구 영역 유동인구 정보 추출")
+						.query(FLOW_POP, gangnaum)
+						.expand("year:int", "year = std_ym.substring(0,4)")
+						.aggregateByGroup(Group.ofKeys("block_cd,year").tags("the_geom"),
+											AVG("avg_08tmst").as("avg_08tmst"),
+											AVG("avg_15tmst").as("avg_15tmst"))
+						.project("*-{year}")
+						.store(RESULT, FORCE(gcInfo))
+						.build();
+		marmot.execute(plan);
+		DataSet result = marmot.getDataSet(RESULT);
+		watch.stop();
+		
+		// 결과에 포함된 일부 레코드를 읽어 화면에 출력시킨다.
+		SampleUtils.printPrefix(result, 5);
+		System.out.printf("elapsed=%s%n", watch.getElapsedMillisString());
+	}
+	
+	private static Geometry getGangnamGu(MarmotRuntime marmot) {
+		Plan plan;
+		plan = marmot.planBuilder("강남구 추출")
+					.load(SGG)
+					.filter("sig_cd.startsWith('11') && sig_kor_nm == '강남구'")
+					.project("the_geom")
+					.build();
+		return marmot.executeLocally(plan).toList().get(0).getGeometry("the_geom");
+	}
+}
